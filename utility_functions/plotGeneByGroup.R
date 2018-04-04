100c4f83b9cb8d28a6d3fb367ee2c0432ba15cc7
Adam Pennycuick <adampennycuick@gmail.com>
2018-04-04 14:19:18 +0100

    Change density plot to boxplot
    plotGeneByGroup function now uses a box plot rather than density plot.

--- utility_functions/plotGeneByGroup.R
+++ utility_functions/plotGeneByGroup.R
@@ -1,28 +1,22 @@
 # Function to plot a gene over different 'dose' groups
 plotGeneByGroup <- function(gene, legend.pos='topright'){
-  d0 <- density(as.numeric(tcga.gdata.all[gene,which(tcga.gpheno.all$dose == 0)]))
-  d1 <- density(as.numeric(tcga.gdata.all[gene,which(tcga.gpheno.all$dose == 1)]))
-  d2 <- density(as.numeric(tcga.gdata.all[gene,which(tcga.gpheno.all$dose == 2)]))
-  d3 <- density(as.numeric(tcga.gdata.all[gene,which(tcga.gpheno.all$dose == 3)]))
-  #d4 <- density(as.numeric(tcga.gdata.all[gene,which(tcga.gpheno.all$dose == -1)]))
-  plot(
-    d0, 
-    col="darkgreen",
-    xlim=c(
-      min(as.numeric(tcga.gdata.all[gene,])), max(as.numeric(tcga.gdata.all[gene,]))
-    ),
-    xlab="Gene Expression Raw Value",
-    ylim=c(0,max(d0$y, d1$y, d2$y, d3$y)),
-    main=gene
-  )
-  lines(d1, col='green')
-  lines(d2, col='red')
-  lines(d3, col='orange')
-  #lines(d4, col='black')
-  if(show.legends){
-    legend(legend.pos, 
-           c("TCGA.cont", "CIS.reg", "CIS.prog", "TCGA.LUSC"),
-           col=c("darkgreen", "green", "red", "orange"), lty=1)
-  }
+  
+  colorder <- c( "darkgreen", "green", "red", "orange")
+  stages <- c("TCGA control", "Regressive CIS", "Progressive CIS", "TCGA SqCC")
+  
+  bpdata <- tcga.gpheno.all
+  bpdata$gene <- as.numeric(tcga.gdata.all[gene,])
+  bpdata$stage <- factor(stages[tcga.gpheno.all$dose + 1], levels=stages)
+  bplot <- ggplot(bpdata, aes(x=factor(stage),y=gene, fill=factor(stage))) +
+    geom_boxplot() +
+    geom_signif(comparisons=list(
+      c(stages[1], stages[2]), c(stages[2], stages[3]), c(stages[3], stages[4])
+    ), map_signif_level = TRUE) +
+    ggtitle(paste(gene, "Expression")) + 
+    scale_fill_manual(values=colorder) + 
+    theme(axis.title.x=element_blank()) +
+    theme(legend.position="none") +
+    scale_y_continuous(name=gene)
+  print(bplot)
   
 }
